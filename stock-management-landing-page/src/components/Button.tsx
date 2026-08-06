bfd12a9802ea5c3f8d8af75dd8df8c05d6fbd63a
MarguJ <margulesjoa@gmail.com>
2026-08-06 15:51:09 -0300

    Button and input created

---
+++ stock-management-landing-page/src/components/Button.tsx
@@ -0,0 +1,5 @@
+export function Button() {
+    return(
+        <button className="w-30 h-20 bg-white">SOY UN BOTONNNNNN</button>
+    )
+}
---
+++ stock-management-landing-page/src/components/Input.tsx
@@ -0,0 +1,5 @@
+export function Input() {
+    return(
+        <input className="w-30 h-20 bg-white"></input>
+    )
+}
