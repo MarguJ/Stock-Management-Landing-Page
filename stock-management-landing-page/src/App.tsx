a0b6c5db53f6d33be6dafc56d4d3551bd0363911
MarguJ <margulesjoa@gmail.com>
2026-08-11 14:24:03 -0300

    Lol no hace cuanto

--- stock-management-landing-page/src/App.tsx
+++ stock-management-landing-page/src/App.tsx
@@ -1,12 +1,12 @@
-import { Button } from './components/button'
+import { Button } from './components/Button'
 import { Input } from './components/Input'
 import './index.css'
 
 function App() {
   return (
-    <div className='bg-black min-w-screen min-h-screen'>
-    <Input></Input>
+    <div className='bg-gray-400 min-w-screen min-h-screen'>
     <Button></Button>
+    <Input></Input>
     </div>
   )
 }
--- stock-management-landing-page/src/components/Button.tsx
+++ stock-management-landing-page/src/components/Button.tsx
@@ -1,5 +1,5 @@
 export function Button() {
     return(
-        <button className="w-30 h-20 bg-white">SOY UN BOTONNNNNN</button>
+        <button className="w-30 h-20 bg-white"></button>
     )
 }
