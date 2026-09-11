a8abb5344f8db1e494f2b63247d90198887699e8
MarguJ <margulesjoa@gmail.com>
2026-09-11 18:43:42 -0300

    Commit THIS

--- stock-management-landing-page/src/App.tsx
+++ stock-management-landing-page/src/App.tsx
@@ -1,24 +1,22 @@
-import './index.css'
+import "./index.css";
 
 function App() {
   return (
-    <div className='bg-gray-400 min-w-screen min-h-screen'>
-      <section id='1'  className='welcome'>
-        <div className='bg-white min-h-160 w-200'>
-          <img src="./images/pasillo.jfif"/>
+    <div className="bg-gray-400 min-w-screen min-h-screen">
+      <section id="1" className="welcome">
+        <div className="bg-white min-h-160 w-200">
+          <img src="./images/pasillo.jpg" />
         </div>
       </section>
-      <section id='2' className='about us'>
-        <div className='font-[Kdam Thmor Pro] bg-red-600 min-w-screen min-h-40 text-white text-7xl flex items-center justify-center'>¿Quienes somos?</div>
+      <section id="2" className="about us">
+        <div className="font-[Kdam Thmor Pro] bg-red-600 min-w-screen min-h-40 text-white text-7xl flex items-center justify-center">
+          ¿Quienes somos?
+        </div>
       </section>
-      <section id='3' className='app facts'>
-
-      </section>
-      <section id='4' className='contact us'>
-
-      </section>
+      <section id="3" className="app facts"></section>
+      <section id="4" className="contact us"></section>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
