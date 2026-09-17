c34aea42fc7befb389cbe574e6a108abaa99f0ee
MarguJ <margulesjoa@gmail.com>
2026-09-17 15:49:19 -0300

    amigo no se van para el otro lado

--- stock-management-landing-page/src/App.tsx
+++ stock-management-landing-page/src/App.tsx
@@ -3,17 +3,17 @@ import { Navbar } from "./components/Navbar";
 
 function App() {
   return (
-    <div className="bg-gray-400 min-w-screen min-h-screen">
+    <div className="bg-gray-400 min-h-screen">
       <header>
         <Navbar></Navbar>
       </header>
       <section id="1" className="welcome">
-        <div className="bg-gray-450 min-h-screen min-w-screen">
+        <div className="bg-gray-450">
           <img className="min-h-screen" src="../images/pasillo.jpg" />
         </div>
       </section>
       <section id="2" className="about us">
-        <div className="font-[Kdam Thmor Pro] bg-red-600 min-w-screen min-h-40 text-white text-7xl flex items-center justify-center">
+        <div className="font-[Kdam Thmor Pro] bg-red-600 min-h-40 text-white text-7xl flex items-center justify-center">
           ¿Quienes somos?
         </div>
       </section>
--- stock-management-landing-page/src/components/Navbar.tsx
+++ stock-management-landing-page/src/components/Navbar.tsx
@@ -1,7 +1,16 @@
 export function Navbar() {
   return (
-    <nav className="bg-red-600 text-white font-bold fixed top-0 h-15 min-w-screen">
-      <img className="" src="../images/300x300.png"></img>
+    <nav className="bg-white text-black fixed top-0 h-15 min-w-screen flex row-span">
+      <div className="flex justify-start items-center ">
+        <img className="w-30 ml-5" src="../images/fischerLogo.png"></img>
+        <img className="ml-2 h-10" src="../images/separacion.png"></img>
+        <div className="w-50 ml-4">Stock Management Software</div>
+      </div>
+      <div className="flex justify-end items-center">
+          <div>Caracteristicas</div>
+          <div>Beneficios</div>
+          <div>Empresa</div>
+      </div>
     </nav>
   );
 }
