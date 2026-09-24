b49cb7ad1bcd7a16892b08fa605d497751d0249b
MarguJ <margulesjoa@gmail.com>
2026-09-24 15:25:07 -0300

    EA AHORA SIIII

--- stock-management-landing-page/src/components/HowItWorks.tsx
+++ stock-management-landing-page/src/components/HowItWorks.tsx
@@ -1,17 +1,16 @@
-const steps = [
-  "La app de Fischer Stock Management simplifica muchísimo el recorrido de relevamiento, ahorrando tiempo de trabajo para los empleados y por lo tanto plata para tu empresa.",
-  "No solo ahorras todo eso, sino que también podes monitorear desde tu celular un ranking de productos filtrado como vos quieras y una lista de empleados con cada visita con fecha y lugar.",
-  "Siendo administrador en nuestra aplicación podes monitorear, crear, modificar y eliminar tanto productos como usuarios solamente con unos clicks.",
-];
 export function HowItWorks() {
   return (
     <section className="how-it-works" id="como-funciona">
       <h2>¿CÓMO FUNCIONA?</h2>
       <ol>
-        {steps.map((step, index) => (
-          <li key={index}>{step}</li>
-        ))}
+        <li>La app de <strong>Fischer Stock Management</strong> simplifica muchísimo el recorrido de relevamiento, ahorrando tiempo de trabajo para los empleados y por lo tanto plata para tu empresa.</li>
+        <li>No solo ahorras todo eso, sino que también podes monitorear desde tu celular un ranking de productos filtrado como vos quieras y una lista de empleados con cada visita con fecha y lugar.</li>
+        <li>Siendo administrador en nuestra aplicación podes monitorear, crear, modificar y eliminar tanto productos como usuarios solamente con unos clicks.</li>
       </ol>
     </section>
   );
 }
+
+/*{steps.map((step, index) => (
+  <li key={index}>{step}</li>
+))} */
